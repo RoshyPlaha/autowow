3d2f57e12ef9845666a9a463243c694c6d0f906b
Roshy <roshsplaha@gmail.com>
2025-11-15 22:35:38 +0000

    add opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,18 +34,18 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://capsules.today'),
+  metadataBase: new URL('https://autowow.vercel.app/'),
   title: {
     default: "Autowow",
     template: "%s | Autowow",
   },
-  description: "Record with intent. Time. moves. fast. Capsule your moments now.",
+  description: "Find your next car in seconds",
   twitter: {
     title: 'Autowow - Search your next car',
     card: 'summary_large_image',
     images: [
       {
-        url: '/assets/twitter-share.png',
+        url: 'assets/opengraph.png',
         width: 1200,
         height: 522,
         alt: 'Autowow - Search your next car',
@@ -55,8 +55,8 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'CAutowow - Search your next car',
     description: "Search. Scroll. Find your next car.",
-    url: 'https://capsules.today',
-    siteName: 'Capsules',
+    url: 'https://autowow.vercel.app/',
+    siteName: 'Autowow',
     locale: 'en_GB',
     type: 'website',
     images: [
@@ -64,7 +64,7 @@ export const metadata: Metadata = {
         url: '/assets/twitter-share.png',
         width: 1200,
         height: 522,
-        alt: 'Capsules - Record Future Memories',
+        alt: 'Autowow - Find your next car',
       },
     ],
   }
