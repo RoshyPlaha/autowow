36d8ce2c4eb6f44715c788012da6d5befe596770
Roshy <roshsplaha@gmail.com>
2026-01-18 17:49:41 +0000

    updated sitemap to index pages

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,15 +1,13 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-    return {
-        rules: [
-            {
-                userAgent: "*",
-                allow: "/",
-                disallow: ["/pricing", "/account"]
-
-            }
-        ],
-        sitemap: "https://autoro.space/sitemap.xml",
-    }
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://autoro.space/sitemap.xml",
+  };
 }
