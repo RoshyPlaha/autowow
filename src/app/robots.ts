5fe380525febe191e092507f2ea81359b4760263
Roshy <roshsplaha@gmail.com>
2026-01-18 17:49:04 +0000

    updated sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -10,6 +10,6 @@ export default function robots(): MetadataRoute.Robots {
 
             }
         ],
-        sitemap: "https://capsules.today/sitemap.xml",
+        sitemap: "https://autoro.space/sitemap.xml",
     }
 }
